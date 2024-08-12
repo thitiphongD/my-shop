6b7253a5e6de66b9218390197e37d920beda1845
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-12 20:09:55 +0700

    color mock

--- src/app/[locale]/components/Banner.tsx
+++ src/app/[locale]/components/Banner.tsx
@@ -2,6 +2,10 @@ import { Carousel } from "antd";
 import React from "react";
 import "../css/home.css";
 
+// #febd11
+
+// #43ccd4
+
 const Banner = () => {
   return (
     <Carousel autoplay>
@@ -9,13 +13,10 @@ const Banner = () => {
         <h3 className="banner">1</h3>
       </div>
       <div>
-        <h3 className="banner">2</h3>
+        <h3 className="banner2">2</h3>
       </div>
       <div>
-        <h3 className="banner">3</h3>
-      </div>
-      <div>
-        <h3 className="banner">4</h3>
+        <h3 className="banner3">3</h3>
       </div>
     </Carousel>
   );
